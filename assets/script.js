c3301a940f754a1823afa4a16e2a2901a6000b6c
PurviMec <princy.mecwan94@gmail.com>
2021-09-18 17:24:11 -0400

    update quiz game

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,16 @@ const questionContainerEl = document.getElementById('question-container')
 const questionContainer2El = document.getElementById('question-container2')
 const questionContainer3El = document.getElementById('question-container3')
 const questionContainer4El = document.getElementById('question-container4')
-const scoreEl = document.getElementById('score')
+const scoreContainerEl = document.getElementById('score-container')
+const highScoreContainerEl = document.getElementById('highscore-container')
+
 
 let startingMinutes = 1;
 let time = startingMinutes * 60;
 
+let numCorrect = 0;
+
+let correctAnswer1 = document.getElementById('ans1c')
 
 //start countdown and jump to que 1 by clicking start button
 var startGame = function(){
@@ -29,34 +34,57 @@ var startGame = function(){
     questionContainerEl.style.display = "block";
 
     
-    
 };
+
 
 function selectAnswer(){
     questionContainerEl.style.display = "none"; 
     questionContainer2El.style.display = "block";
     
+    var correct1 = document.getElementById("ans1c")
+    var wrong1 = document.getElementById('ans1a', 'ans1b', 'ans1d')
 
+
+        if (correct1.clicked === true){
+            numCorrect +=5;
+        }
+        else{
+            time -= 5;
+        }
+        
 }
+
+
 
 function selectAnswer2(){
     questionContainer2El.style.display = "none"; 
     questionContainer3El.style.display = "block";
+
+    
 }
 
 function selectAnswer3(){
     questionContainer3El.style.display = "none"; 
     questionContainer4El.style.display = "block";
+
+
 }
 
 function selectAnswer4(){
     questionContainer4El.style.display = "none"; 
-    scoreEl.style.display = "block";
+    scoreContainerEl.style.display= "block";
 }
 
-function finalScore(){
-    scoreEl.style.display = "block";
+function score(){
+    scoreContainerEl.style.display = "none";
+    highScoreContainerEl.style.display = "block";
+
 }
+
+function highScore(){
+
+}
+
 
 
 
@@ -66,7 +94,11 @@ questionContainerEl.addEventListener("click",selectAnswer)
 questionContainer2El.addEventListener("click",selectAnswer2)
 questionContainer3El.addEventListener("click",selectAnswer3)
 questionContainer4El.addEventListener("click",selectAnswer4)
-scoreEl.addEventListener("click",finalScore)
+scoreContainerEl .addEventListener("click", score)
+highScoreContainerEl.addEventListener("click", highScore)
+
+
+
 
 
 
